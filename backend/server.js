677374501a5c2a14ae2c15c108caa364191f0f1b
alaaayman77 <alaaaymanfouad@gmail.com>
2025-06-02 13:08:15 +0300

    otp_fixed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const usersRouter = require("./routes/usersRoutes");
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5175",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
