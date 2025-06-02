284841e8856fab0c951f3b722115f2b5f447328d
alaaayman77 <alaaaymanfouad@gmail.com>
2025-06-02 13:09:27 +0300

    cors_url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const usersRouter = require("./routes/usersRoutes");
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:5175",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
